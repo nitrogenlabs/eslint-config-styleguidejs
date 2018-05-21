1dbbfe3066c2b36a22d366ecd3e3fdd7e6784465
Giraldo Rosales <giraldo@nitrogenlabs.com>
2018-05-20 19:44:45 -0500

    Add typescript rules

--- typescript.js
+++ typescript.js
@@ -1,9 +1,23 @@
 module.exports = {
-  extends: path.resolve(__dirname, './base'),
-  parser: path.resolve(__dirname, './node_modules/typescript-eslint-parser'),
+  extends: 'eslint-config-styleguidejs/base',
+  parser: 'typescript-eslint-parser',
   plugins: [
-    path.resolve(__dirname, './node_modules/eslint-plugin-typescript')
+    'typescript'
   ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
   rules: {
+    'no-undef': 0,
+    'typescript/class-name-casing': 2,
+    'typescript/explicit-function-return-type': 2,
+    'typescript/member-naming': 2,
+    'typescript/member-naming': 2,
+    'typescript/member-ordering': 2,
+    'typescript/no-angle-bracket-type-assertion': 2,
+    'typescript/no-array-constructor': 2,
+    'typescript/no-unused-vars': 2,
+    'typescript/type-annotation-spacing': 2
   }
 }
